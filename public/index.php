77c539d02c51d54766168fb35168e0d546909548
Mossko <moktarbabadrame@gmail.com>
2026-06-23 10:15:23 +0200

    Modif apportés

--- public/index.php
+++ public/index.php
@@ -3,9 +3,14 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 define('APP_ROOT', dirname(__DIR__));
-define('APP_ENV', ".env.local.");
+define('APP_ENV', ".env.local");
 
 use App\routing\Router;
 
 $router = new Router();
 $router->handleRequest($_SERVER["REQUEST_URI"]);
+
+use App\Db\Mysql;
+
+$mysql = Mysql::getInstance();
+$mysql->getPDO();
